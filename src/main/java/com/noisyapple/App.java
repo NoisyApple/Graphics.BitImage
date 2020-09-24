713ea00cdf774501f998460dc63e94c5b89d34d1
NoisyApple <leonel.aguirre77@gmail.com>
2020-09-24 09:08:46 -0500

    Figure drawing and draw loop working

---
+++ src/main/java/com/noisyapple/App.java
@@ -0,0 +1,9 @@
+package com.noisyapple;
+
+public class App {
+    public static void main(String[] args) {
+        new Drawing(500, 500);
+        // Drawing d = new Drawing();
+        // d.draw();
+    }
+}
---
+++ src/main/java/com/noisyapple/Drawing.java
@@ -0,0 +1,117 @@
+package com.noisyapple;
+
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Drawing extends JFrame {
+
+  // private final int[] FIGURE = { 0x9C9C9C9C, 0xC9C9C9C9, 0x63636363,
+  // 0x36363636, 0x63636363, 0xC9C9C9C9, 0x9C9C9C9C,
+  // 0xC9C9C9C9, 0x63636363, 0x36363636, 0x63636363, 0xC9C9C9C9, 0x9C9C9C9C,
+  // 0xC9C9C9C9, 0x63636363 };
+
+  private final int[] FIGURE = { 0x1FF80FE0, 0x1FFC1FF0, 0x01DC1C38, 0x18DE3F18, 0x18CE3B18, 0x19C77338, 0x1F8773F0,
+      0x1F03E3E0, 0x0001C000, 0x00008000, 0x001FF800, 0x03FFFFC0, 0x1FFFFFF8, 0x7FFC3FFE, 0xFFE007FF, 0x7FFC3FFE,
+      0x1FFFFFF8, 0x03FFFFC0, 0x001FF800 };
+
+  private int UPDATE_RATE = 60;
+  private final int MASK = 0x80000000;
+  private final int BIT_LENGTH = 32;
+  private final int PIXEL_SIZE = 5;
+
+  private int width, height;
+  private Color color;
+
+  private int xSpeed = 1;
+  private int ySpeed = 1;
+
+  private int xPos = 0;
+  private int yPos = 0;
+
+  public Drawing(int width, int height) {
+    this.width = width;
+    this.height = height;
+    this.generateColor();
+
+    this.setTitle("Bit Drawing");
+    this.setVisible(true);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setResizable(false);
+
+    this.add(new CustomCanvas());
+
+    this.pack();
+    this.setLocationRelativeTo(null);
+
+    startLoop();
+  }
+
+  public void generateColor() {
+    this.color = Color.getHSBColor((float) Math.random(), 0.8f, 0.8f);
+  }
+
+  public void startLoop() {
+    Thread drawLoop = new Thread() {
+      public void run() {
+        while (true) {
+          repaint();
+
+          xPos += xSpeed;
+          yPos += ySpeed;
+
+          if (xPos + BIT_LENGTH * PIXEL_SIZE > width || xPos < 0) {
+            xSpeed *= -1;
+            generateColor();
+          }
+
+          if (yPos + FIGURE.length * PIXEL_SIZE > height || yPos < 0) {
+            ySpeed *= -1;
+            generateColor();
+          }
+
+          try {
+            Thread.sleep(1000 / UPDATE_RATE);
+          } catch (InterruptedException ex) {
+          }
+        }
+      }
+    };
+
+    drawLoop.start();
+  }
+
+  class CustomCanvas extends JPanel {
+
+    public void paintComponent(Graphics g) {
+
+      g.setColor(Color.decode("#000000"));
+
+      g.fillRect(0, 0, width, height);
+
+      g.setColor(color);
+
+      System.out.println("X: " + xPos + ", Y: " + yPos + ", X_SPEED: " + xSpeed + ", Y_SPEED: " + ySpeed);
+
+      for (int figure = 0; figure < FIGURE.length; figure++) {
+        for (int bit = 0; bit < BIT_LENGTH; bit++) {
+          int temp = FIGURE[figure] & (MASK >>> bit);
+
+          if (temp != 0) {
+            g.fillRect(bit * PIXEL_SIZE + xPos, figure * PIXEL_SIZE + yPos, PIXEL_SIZE, PIXEL_SIZE);
+
+          }
+        }
+      }
+    }
+
+    public Dimension getPreferredSize() {
+      return (new Dimension(width, height));
+    }
+
+  }
+
+}
